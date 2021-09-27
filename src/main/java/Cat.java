44e4d4dff6951b9bd5c922dc4014a75e75dd339c
Bradley Mathi <bradley.mathi@mail.utoronto.ca>
2021-09-27 11:48:27 -0400

    added toString and documentation.

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -3,10 +3,18 @@ public class Cat implements Domesticatable, Tradable{
 
     public Cat() {}
 
+    /* Returns the sound that the cat makes.*/
     @Override
     public String sound() {return "Meow!";}
 
+    /* Returns the price of the cat.*/
     @Override
     public int getPrice() {return 3;}
+
+    /* Returns a String representation of the cat.*/
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
 }
